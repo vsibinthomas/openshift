5ba233ff400e5fa3eb02e82045038bcd75022fde
Sibin Thomas Valliamannil <stv30@itl205.student.eecs.qmul.ac.uk>
2017-03-07 12:17:19 +0000

    second commit sad happy ones

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,5 +1,29 @@
 from app import app
+from flask import render_template, request
+import unirest
+from forms import MessageForm
+
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
-	return "Hello, World!"
+    return render_template("index.html")
+
+@app.route('/emotion/')
+def emotion():
+	return render_template("my_form.html",mood='happy',form=MessageForm())
+
+@app.route('/emotion/', methods=['POST'])
+def emotion_post():
+	msg = request.form['message']
+	response = unirest.post("https://community-sentiment.p.mashape.com/text/",
+	  headers={
+	    "X-Mashape-Key": "6VWQcE5umumsh9oLsHfFlOseFGbDp1caaUKjsnj6PJRqxZKslv",
+	    "Content-Type": "application/x-www-form-urlencoded",
+    	"Accept": "application/json"
+    	},
+  		params={
+    	"txt": msg
+  		}
+	)
+	return render_template("my_form.html",mood=response.body['result']['sentiment'],form=MessageForm())
+
