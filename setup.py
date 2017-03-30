becb4cabb9443d328b09e516ea101c466f7a02fe
Sibin Thomas Valliamannil <stv30@dil079.student.eecs.qmul.ac.uk>
2017-03-30 17:44:39 +0100

    added pymongo and flask_pymongo

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='stv30apitest1',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='Sibin Thomas',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['MarkupSafe','Flask','flask-wtf','flask-babel','markdown','flup','unirest','Flask-PyMongo','bokeh'],
+      install_requires=['MarkupSafe','Flask','flask-wtf','flask-babel','markdown','flup','unirest','Flask-PyMongo','pymongo','flask_pymongo','bokeh','flask_navigation'],
       )
