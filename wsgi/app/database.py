6a664562fb0705caa41458ce343afdaec5af0d68
Sibin Thomas Valliamannil <stv30@dil079.student.eecs.qmul.ac.uk>
2017-03-30 19:48:34 +0100

    testing again with new database csv

--- wsgi/app/database.py
+++ wsgi/app/database.py
@@ -19,3 +19,19 @@ def get_all_personnel():
 		output.append({'Who' : doc['Name'], 'Job Role' : doc['Profession']})
 	return jsonify({'result' : output})
 
+@app.route('/database/methods', methods=['GET'])
+def get_db_methods_and_attributes():
+	return jsonify({'Methods & attributes on a flask mongodb obj' : dir(mongo.db)})
+
+@app.route('/database/London/methods', methods=['GET'])
+def get_collection_methods_and_attributes():
+	return jsonify({'Methods of a flask mgdb collection obj' : dir(mongo.db.London)})
+
+@app.route('/database/London/sample', methods=['GET'])
+def get_sample_document():
+	collection = mongo.db.London
+	doc = collection.find_one()
+	output = []
+	output.append({attr:value for attr, value in doc.iteritems() if attr!=u'_id'})
+	return jsonify({'sample record' : output})
+
